8ec1323efa7f2e8c33f6dc81021b8a2bf6125bde
atman <gpbjk0304@gmail.com>
2024-08-01 23:58:01 +0900

    feat: 見栄えを見直し
    tailwindを適用して初期作成していたページのレイアウトが崩れたため見直し

--- src/renderer/src/pages/index.tsx
+++ src/renderer/src/pages/index.tsx
@@ -3,7 +3,9 @@ import { Link } from 'react-router-dom';
 const IndexPage = () => {
   return (
     <>
-      <Link to="/debug">Debu page</Link>
+      <Link to="/debug">
+        <div className="text-blue-600 underline">Debug page</div>
+      </Link>
     </>
   );
 };
